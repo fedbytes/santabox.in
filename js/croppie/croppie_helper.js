1ecbdd01e9f05680a573eebc67631cb6b3b9ea32
Fedbytes Technologies <info.fedbytes@gmail.com>
2021-11-21 15:01:35 +0530

    added

---
+++ js/croppie/croppie_helper.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){    
+	$image_crop = $('#upload-image').croppie({
+		enableExif: true,
+		viewport: {
+			width: 200,
+			height: 200,
+			type: 'circle',
+			quality: 0.6
+		},
+		boundary: {
+			width: 300,
+			height: 300
+		}
+	});
+	$('#set_pic').hide();
+	$('#box-image').on('change', function () { 
+		var reader = new FileReader();
+		reader.onload = function (e) {
+			$image_crop.croppie('bind', {
+				url: e.target.result
+			}).then(function(){
+				$('#set_pic').show();
+			});			
+		}
+		reader.readAsDataURL(this.files[0]);
+	});
+	$('#set_pic').on('click',function() {
+		$image_crop.croppie('result', {
+			type: 'canvas',
+			size: 'viewport',
+			quality: 0.6
+		}).then(function (response) {
+			$('#box-img').attr('src',response);
+			$("#croppieModal").modal('hide');
+		});
+	})
+	$('#cancel_pic').on('click',function() {
+		$("#croppieModal").modal('hide');
+	})
+
+	$('#select_pic').on('click',function() {
+		$('#box-image').trigger('click');
+	})
+});
